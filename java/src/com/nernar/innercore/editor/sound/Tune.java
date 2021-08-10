20d8fa2bdc8d6e7406401c9ff738945e905713bb
MaXFeeD <46795182+MaXFeeD@users.noreply.github.com>
2021-08-11 00:29:19 +0300

    Moved text typer effect to java

---
+++ java/src/com/nernar/innercore/editor/sound/Tune.java
@@ -0,0 +1,206 @@
+/*
+
+   Copyright 2021 Nernar (github.com/nernar)
+   
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+   
+       http://www.apache.org/licenses/LICENSE-2.0
+   
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*/
+package com.nernar.innercore.editor.sound;
+
+import android.media.*;
+
+public class Tune {
+	
+	private final int mBufferSize;
+	private final AudioTrack mTrack;
+	private final int mSampleRateInHz;
+	
+	public Tune(int sampleRateInHz, int channelOut, int encoding, int streamType, int mode) {
+		mBufferSize = AudioTrack.getMinBufferSize(sampleRateInHz,
+												  channelOut, encoding);
+		mTrack = new AudioTrack(streamType, sampleRateInHz,
+								channelOut, encoding, mBufferSize, mode);
+		mSampleRateInHz = sampleRateInHz;
+	}
+	
+	public void write(TrackBuffer tone) {
+		mTrack.write(tone.getBuffer(), 0, tone.getBufferSize());
+	}
+	
+	public void write(short hz) {
+		Tone tone = createTone();
+		tone.write(hz);
+		write(tone);
+	}
+	
+	public void write(short min, short max) {
+		Tone tone = createTone();
+		tone.write(min, max);
+		write(tone);
+	}
+	
+	public void writeStatic(short min, short max) {
+		Tone tone = createTone();
+		tone.writeStatic(min, max);
+		write(tone);
+	}
+	
+	public void writeStatic(short hz, int affinity) {
+		Tone tone = createTone();
+		tone.writeStatic(hz, affinity);
+		write(tone);
+	}
+	
+	public void play() {
+		mTrack.play();
+	}
+	
+	public void pause() {
+		mTrack.pause();
+	}
+	
+	public void stop() {
+		mTrack.stop();
+	}
+	
+	public void release() {
+		mTrack.release();
+	}
+	
+	public int getBufferSize() {
+		return mBufferSize;
+	}
+	
+	public int getSampleRateInHz() {
+		return mSampleRateInHz;
+	}
+	
+	public AudioTrack getAudioTrack() {
+		return mTrack;
+	}
+	
+	public static class Builder {
+		
+		private int mOut = AudioFormat.CHANNEL_OUT_MONO;
+		private int mEncoding = AudioFormat.ENCODING_PCM_16BIT;
+		private int mStream = AudioManager.STREAM_MUSIC;
+		private int mMode = AudioTrack.MODE_STREAM;
+		private int mSampleRateInHz = 4000;
+		
+		public void setChannelOut(int out) {
+			mOut = out;
+		}
+		
+		public void setEncoding(int encoding) {
+			mEncoding = encoding;
+		}
+		
+		public void setStreamType(int type) {
+			mStream = type;
+		}
+		
+		public void setMode(int mode) {
+			mMode = mode;
+		}
+		
+		public void setSampleRate(int hz) {
+			mSampleRateInHz = hz;
+		}
+		
+		public Tune create() {
+			return new Tune(mSampleRateInHz, mOut, mEncoding, mStream, mMode);
+		}
+	}
+	
+	public static interface TrackBuffer {
+		
+		public int getBufferSize();
+		
+		public short[] getBuffer();
+	}
+	
+	public static class Tone implements TrackBuffer {
+		
+		public final double TWOPI = 8. * Math.atan(1.);
+		private final int mSampleRateInHz;
+		private final short[] mBuffer;
+		
+		public Tone(Tune parent) {
+			this(parent != null ? parent.getBufferSize() : 0, parent != null ? parent.getSampleRateInHz() : 0);
+		}
+		
+		public Tone(int bufferSize, int sampleRate) {
+			this(bufferSize > 0 ? new short[bufferSize] : null, sampleRate);
+		}
+		
+		public Tone(short[] buffer, int sampleRate) {
+			if (buffer == null) {
+				throw new IllegalArgumentException("Buffer must be specified");
+			}
+			if (sampleRate <= 0) {
+				throw new IllegalArgumentException("Sample rate mist be > 0");
+			}
+			mBuffer = buffer;
+			mSampleRateInHz = sampleRate;
+		}
+		
+		public void write(short hz) {
+			synchronized (mBuffer) {
+				double ph = 0.0;
+				for (int i = 0; i < getBufferSize(); i++) {
+					ph += TWOPI * hz / mSampleRateInHz;
+					writeAt(i, (short) Math.sin(ph));
+				}
+			}
+		}
+		
+		public void write(short min, short max) {
+			write((short) (min + Math.random() * (max - min)));
+		}
+		
+		public void writeStatic(short min, short max) {
+			synchronized (mBuffer) {
+				double ph = 0.0;
+				for (int i = 0; i < getBufferSize(); i++) {
+					ph += TWOPI * (min + Math.random() * (max - min)) / mSampleRateInHz;
+					writeAt(i, (short) Math.sin(ph));
+				}
+			}
+		}
+		
+		public void writeStatic(short hz, int affinity) {
+			writeStatic((short) (hz - affinity), (short) (hz + affinity));
+		}
+		
+		public synchronized void writeAt(int index, short hz) {
+			if (index < 0 || index >= getBufferSize()) {
+				throw new IndexOutOfBoundsException("Sample out of range [0.." + getBufferSize() + "]: " + index);
+			}
+			mBuffer[index] = (short) (10000 * hz);
+		}
+		
+		@Override
+		public int getBufferSize() {
+			return mBuffer.length;
+		}
+		
+		@Override
+		public short[] getBuffer() {
+			return mBuffer;
+		}
+	}
+	
+	public Tone createTone() {
+		return new Tone(this);
+	}
+}
---
+++ java/src/com/nernar/innercore/editor/widget/ToneTypingTextView.java
@@ -0,0 +1,162 @@
+/*
+
+   Copyright 2021 Nernar (github.com/nernar)
+   
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+   
+       http://www.apache.org/licenses/LICENSE-2.0
+   
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*/
+package com.nernar.innercore.editor.widget;
+
+import android.content.*;
+import android.media.*;
+import android.support.annotation.*;
+import android.util.*;
+
+public class ToneTypingTextView extends TypingTextView {
+	
+	private final static int INTERNAL_TONE_COUNT = 98;
+	private int mDefaultTone = getRandomTone();
+	private int mDefaultToneDuration = 20;
+	private ToneGenerator mToneGenerator;
+	private OnResolveToneObserver observer;
+	
+	public ToneTypingTextView(Context context) {
+		super(context);
+	}
+	
+    public ToneTypingTextView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+	
+    public ToneTypingTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+	}
+	
+	public static abstract interface OnResolveToneObserver {
+		
+		public abstract boolean onResolveCharacterToneable(char sub);
+		
+		public abstract int onResolveCharacterTone(int base, char sub);
+		
+		public abstract int onResolveCharacterToneDuration(int base, char sub);
+	}
+	
+	protected void rebuildToneGenerator() {
+		if (mToneGenerator != null) {
+			return;
+		}
+		synchronized (ToneGenerator.class) {
+			try {
+				mToneGenerator = new ToneGenerator(AudioManager.STREAM_MUSIC, ToneGenerator.MAX_VOLUME);
+			} catch (RuntimeException e) {
+				// May fail init on low memory
+			}
+		}
+	}
+	
+	protected void releaseToneGenerator() {
+		synchronized (ToneGenerator.class) {
+			if (mToneGenerator != null) {
+				mToneGenerator.release();
+				mToneGenerator = null;
+			}
+		}
+	}
+	
+	public final ToneGenerator getToneGenerator() {
+		return mToneGenerator;
+	}
+	
+	public final int getDefaultTone() {
+		return mDefaultTone;
+	}
+	
+	public void setDefaultTone(int tone) {
+		if (tone < 0 || tone > INTERNAL_TONE_COUNT) {
+			throw new IndexOutOfBoundsException("Passed tone out of range [0.." + INTERNAL_TONE_COUNT + "]");
+		}
+		mDefaultTone = tone;
+	}
+	
+	public final int getDefaultToneDuration() {
+		return mDefaultToneDuration;
+	}
+	
+	public void setDefaultToneDuration(int duration) {
+		if (duration < 0) {
+			throw new IllegalArgumentException("Tone duration must be >= 0");
+		}
+		mDefaultToneDuration = duration;
+	}
+	
+	public void setOnResolveToneObserver(@Nullable OnResolveToneObserver callback) {
+		observer = callback;
+	}
+	
+	public int getRandomTone() {
+		return (int) (Math.random() * INTERNAL_TONE_COUNT);
+	}
+	
+	@Override
+	public void onTypingStarted(CharSequence who) {
+		rebuildToneGenerator();
+		super.onTypingStarted(who);
+	}
+	
+	protected boolean onToneCharacter(char sub) {
+		if (observer != null) {
+			return observer.onResolveCharacterToneable(sub);
+		}
+		return !Character.isSpaceChar(sub);
+	}
+	
+	protected boolean toneCharacter(char sub) {
+		int tone = getDefaultTone();
+		int duration = getDefaultToneDuration();
+		if (observer != null) {
+			tone = observer.onResolveCharacterTone(tone, sub);
+			duration = observer.onResolveCharacterToneDuration(duration, sub);
+		}
+		return mToneGenerator.startTone(tone, duration);
+	}
+	
+	protected boolean toneCharacter(@NonNull CharSequence sub) {
+		if (mToneGenerator == null) {
+			return false;
+		}
+		char toneable = 0;
+		for (int i = 0; i < sub.length(); i++) {
+			char current = sub.charAt(i);
+			if (onToneCharacter(current)) {
+				toneable = current;
+				break;
+			}
+		}
+		if (toneable == 0) {
+			return false;
+		}
+		return toneCharacter(toneable);
+	}
+	
+	@Override
+	protected void onIncreasedSequence(CharSequence sub, CharSequence who) {
+		if (sub != null) toneCharacter(sub);
+		super.onIncreasedSequence(sub, who);
+	}
+	
+	@Override
+	public void onTypingCompleted(int length) {
+		releaseToneGenerator();
+		super.onTypingCompleted(length);
+	}
+}
