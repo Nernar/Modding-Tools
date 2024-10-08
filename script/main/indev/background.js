bb5fefda5a62f3e5a53f5e5d9968125834e096ba
MaXFeeD <maksfeed.mf@gmail.com>
2024-10-08 20:37:12 +0300

    Moved attachBackground to scripts

---
+++ script/main/indev/background.js
@@ -0,0 +1,25 @@
+const attachBackground = function(drawableOrColor) {
+	handle(function() {
+		let unique = new UniqueWindow();
+		unique.TYPE = "BackgroundWindow";
+		unique.setWidth($.ViewGroup.LayoutParams.MATCH_PARENT);
+		unique.setHeight($.ViewGroup.LayoutParams.MATCH_PARENT);
+		let frame = new android.widget.FrameLayout(getContext());
+		if (drawableOrColor === undefined) {
+			drawableOrColor = $.Color.BLACK;
+		}
+		if (!(drawableOrColor instanceof android.graphics.drawable.Drawable)) {
+			drawableOrColor = new android.graphics.drawable.ColorDrawable(drawableOrColor);
+		}
+		frame.setBackgroundDrawable(drawableOrColor);
+		unique.setLayout(frame);
+		unique.attach();
+	});
+};
+
+const deattachBackground = function() {
+	handle(function() {
+		let unique = UniqueHelper.getWindow("BackgroundWindow");
+		if (unique !== null) unique.dismiss();
+	});
+};
