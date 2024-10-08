29abf198ecd3722c9f9ed1ec37838844714d088c
MaXFeeD <maksfeed.mf@gmail.com>
2024-10-08 20:28:01 +0300

    (Horizontal)ScrollFragment.scrollTo(Fragment)

--- script/main/content/fragment/HorizontalScrollFragment.js
+++ script/main/content/fragment/HorizontalScrollFragment.js
@@ -27,12 +27,44 @@ HorizontalScrollFragment.prototype.getContainerScroll = function() {
 	return this.findViewByTag("containerScroll");
 };
 
-HorizontalScrollFragment.parseJson = function(instanceOrJson, json) {
+ScrollFragment.prototype.scrollTo = function(x, y, duration) {
+	if (isAndroid()) {
+		if (duration != null) {
+			let actor = new android.transition.ChangeScroll();
+			actor.setInterpolator(new android.view.animation.AccelerateDecelerateInterpolator());
+			actor.setDuration(duration);
+			this.beginDelayedTransition(actor);
+		}
+		let scroll = this.getContainerScroll();
+		x == null && (x = scroll.getScrollX());
+		y == null && (y = scroll.getScrollY());
+		scroll.scrollTo(x, y);
+	}
+	return this;
+};
+
+HorizontalScrollFragment.prototype.scrollToFragment = function(fragmentOrIndex, duration) {
+	if (!(fragmentOrIndex instanceof Fragment)) {
+		fragmentOrIndex = this.getFragmentAt(fragmentOrIndex);
+	}
+	if (fragmentOrIndex == null) {
+		Logger.Log("Modding Tools: Cannot scroll to fragment " + fragmentOrIndex + ", it was null!", "WARNING");
+		return this;
+	}
+	let container = fragmentOrIndex.getContainer();
+	return this.scrollTo(container.getX(), container.getY(), duration);
+};
+
+HorizontalScrollFragment.prototype.scrollDown = function(duration) {
+	return this.scrollTo(this.getContainerLayout().getMeasuredWidth(), null, duration);
+};
+
+HorizontalScrollFragment.parseJson = function(instanceOrJson, json, preferredFragment) {
 	if (!(instanceOrJson instanceof HorizontalScrollFragment)) {
 		json = instanceOrJson;
 		instanceOrJson = new HorizontalScrollFragment();
 	}
-	return FrameFragment.parseJson.call(this, instanceOrJson, json);
+	return FrameFragment.parseJson.call(this, instanceOrJson, json, preferredFragment);
 };
 
 registerFragmentJson("horizontalScroll", HorizontalScrollFragment);
--- script/main/content/fragment/ScrollFragment.js
+++ script/main/content/fragment/ScrollFragment.js
@@ -28,12 +28,44 @@ ScrollFragment.prototype.getContainerScroll = function() {
 	return this.findViewByTag("containerScroll");
 };
 
-ScrollFragment.parseJson = function(instanceOrJson, json) {
+ScrollFragment.prototype.scrollTo = function(x, y, duration) {
+	if (isAndroid()) {
+		if (duration != null) {
+			let actor = new android.transition.ChangeScroll();
+			actor.setInterpolator(new android.view.animation.AccelerateDecelerateInterpolator());
+			actor.setDuration(duration);
+			this.beginDelayedTransition(actor);
+		}
+		let scroll = this.getContainerScroll();
+		x == null && (x = scroll.getScrollX());
+		y == null && (y = scroll.getScrollY());
+		scroll.scrollTo(x, y);
+	}
+	return this;
+};
+
+ScrollFragment.prototype.scrollToFragment = function(fragmentOrIndex, duration) {
+	if (!(fragmentOrIndex instanceof Fragment)) {
+		fragmentOrIndex = this.getFragmentAt(fragmentOrIndex);
+	}
+	if (fragmentOrIndex == null) {
+		Logger.Log("Modding Tools: Cannot scroll to fragment " + fragmentOrIndex + ", it was null!", "WARNING");
+		return this;
+	}
+	let container = fragmentOrIndex.getContainer();
+	return this.scrollTo(container.getX(), container.getY(), duration);
+};
+
+ScrollFragment.prototype.scrollDown = function(duration) {
+	return this.scrollTo(null, this.getContainerLayout().getMeasuredHeight(), duration);
+};
+
+ScrollFragment.parseJson = function(instanceOrJson, json, preferredFragment) {
 	if (!(instanceOrJson instanceof ScrollFragment)) {
 		json = instanceOrJson;
 		instanceOrJson = new ScrollFragment();
 	}
-	return FrameFragment.parseJson.call(this, instanceOrJson, json);
+	return FrameFragment.parseJson.call(this, instanceOrJson, json, preferredFragment);
 };
 
 registerFragmentJson("scroll", ScrollFragment);
