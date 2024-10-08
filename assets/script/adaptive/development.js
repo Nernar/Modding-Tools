c02fd33d2d3a58f34dfeeff603e933fe5e093ce8
MaXFeeD <maksfeed.mf@gmail.com>
2024-10-08 19:56:07 +0300

    Moved assets/root to assets, added new sequence scripts, etc.

--- assets/script/adaptive/development.js
+++ assets/script/adaptive/development.js
@@ -17,15 +17,13 @@ Translation.addTranslation("Check", {
 MenuWindow.__parseJson = MenuWindow.parseJson;
 
 const getDebugScripts = function() {
-	let files = Files.listFileNames(Dirs.EVALUATE, true),
-		more = Files.listFileNames(Dirs.SCRIPT_ADAPTIVE, true);
-	files = Files.checkFormats(files.concat(more), ".js");
-	for (let i = 0; i < files.length; i++) {
-		let file = files[i];
-		if (REQUIRE.loaded.indexOf(file) != -1) {
+	let evaluates = Files.listFiles(Dirs.EVALUATE, "relative", "js");
+	let adaptives = Files.listFiles(Dirs.SCRIPT_ADAPTIVE, "relative", "js");
+	let files = evaluates && adaptives ? evaluates.concat(adaptives) : (evaluates || adaptives || []);
+	for (let file, i = 0; i < files.length; i++) {
+		if (REQUIRE.loaded.indexOf(file = files[i]) != -1) {
 			if (REQUIRE.results[file] === undefined) {
-				files.splice(i, 1);
-				i--;
+				files.splice(i--, 1);
 			}
 		}
 	}
@@ -44,7 +42,7 @@ MenuWindow.parseJson = function() {
 			RuntimeCodeEvaluate.showSpecifiedDialog();
 		});
 		category.addItem("inspectorType", translate("Check"), function() {
-			CHECKOUT("provider.js", null, function(who) {
+			CHECKOUT("provider.js", function(who) {
 				evaluateScope();
 			});
 		});
@@ -67,38 +65,39 @@ MenuWindow.parseJson = function() {
 		category.addItem("explorerExtensionImage", translate("Pack"), function() {
 			handleThread(function() {
 				if ($.FileTools.exists(Dirs.INTERNAL_UI)) {
-					AsyncSnackSequence.access("packer.dns", [Dirs.INTERNAL_UI, Dirs.ASSET, 192]).assureYield();
+					AsyncSequence.access("packer.dns", [Dirs.INTERNAL_UI, Dirs.ASSET, 192], null, new SnackSequence()).assureYield();
 				}
 				BitmapDrawableFactory.mapDirectory(Dirs.ASSET, true);
 			});
 		});
 		category.addItem("explorerExtensionJson", translate("Locale"), function() {
 			handleThread(function() {
-				let script = Files.listFiles(__dir__ + "script/main/", true);
+				let script = Files.listFiles([__dir__, "script", "main"]);
 				if ($.FileTools.exists(Dirs.SCRIPT_ADAPTIVE)) {
-					AsyncSnackSequence.access("translation.dns", [Files.listFiles(Dirs.SCRIPT_ADAPTIVE, true).concat(script), __dir__ + "script/main/translation.js", __dir__ + "script/"]).assureYield();
+					AsyncSequence.access("translation.dns", [Files.listFiles(Dirs.SCRIPT_ADAPTIVE).concat(script), __dir__ + "script/main/translation.js", __dir__ + "script/"], null, new SnackSequence()).assureYield();
 				} else {
-					AsyncSnackSequence.access("translation.dns", [script, __dir__ + "script/main/translation.js"]).assureYield();
+					AsyncSequence.access("translation.dns", [script, __dir__ + "script/main/translation.js"], null, new SnackSequence()).assureYield();
 				}
-				let file = new java.io.File(__dir__).getParentFile();
+				let file = Files.of(__dir__).getParentFile();
 				let parent = file.listFiles();
 				for (let i = 0; i < parent.length; i++) {
 					let name = ("" + parent[i].getName()).toLowerCase();
 					if (name.indexOf("modding-tools-") != 0 || name == "modding-tools-template") {
 						continue;
 					}
-					let target = Files.listFiles(parent[i].getPath() + "/script/main/", true);
-					AsyncSnackSequence.access("translation.dns", [target.slice().concat(script), parent[i].getPath() + "/script/main/translation.js", file.getPath() + "/", target]).assureYield();
+					let target = Files.listFiles([parent[i], "script", "main"]);
+					AsyncSequence.access("translation.dns", [target.slice().concat(script), parent[i].getPath() + "/script/main/translation.js", file.getPath() + "/", target], null, new SnackSequence()).assureYield();
 				}
 			});
 		});
 		category.addItem("explorerExtensionScript", translate("Compile"), function() {
 			handleThread(function() {
 				if ($.FileTools.exists(Dirs.SCRIPT_ADAPTIVE + "sequence/")) {
-					AsyncSnackSequence.access("script.dns", [Dirs.SCRIPT_ADAPTIVE + "sequence/", Dirs.SCRIPT_REVISION + "sequence/"]).assureYield();
+					AsyncSequence.access("script.dns", [Dirs.SCRIPT_ADAPTIVE + "sequence/", Dirs.SCRIPT_REVISION + "sequence/"], null, new SnackSequence()).assureYield();
 				}
-				let internal = new java.io.File(Dirs.SCRIPT_ADAPTIVE + "bridge.js");
-				if (!internal.exists()) {
+				let internal = Files.of(Dirs.SCRIPT_ADAPTIVE + "bridge.js");
+				if (!Files.isFile(internal)) {
+					log("Modding Tools: Internal bridge is not found, normally it should be recompiled!");
 					return;
 				}
 				let reader = new java.io.FileReader(internal);
@@ -110,21 +109,30 @@ MenuWindow.parseJson = function() {
 	return instanceOrJson;
 };
 
-LaunchSequence.__create = LaunchSequence.create;
-
 let debugAttachBackground = false;
 
-LaunchSequence.create = function() {
-	CHECKOUT("interface/background.js", null, function(who) {
-		if (debugAttachBackground) {
-			attachBackground();
+(function(self) {
+	let action;
+	ControlFragment.CollapsedButton.prototype.resetContainer = function() {
+		self.apply(this, arguments);
+		if (this.patchedToEvaluation) {
+			return;
 		}
-	});
-	this.__create.apply(this, arguments);
-	CHECKOUT("provider.js", null, function(who) {
-		attachEvalButton();
-	});
-};
+		this.patchedToEvaluation = true;
+		this.setOnHoldListener(function(fragment) {
+			if (typeof action == "function") {
+				if (action.apply(this, arguments) == true) {
+					return true;
+				}
+			}
+			RuntimeCodeEvaluate.showSpecifiedDialog();
+			return true;
+		});
+		this.setOnHoldListener = function(what) {
+			action = what;
+		};
+	};
+})(ControlFragment.CollapsedButton.prototype.resetContainer);
 
 Translation.addTranslation("If you're wouldn't see development panel here, it may be removed.", {
 	ru: "Если панель разработчика сейчас не нужна, она может быть удалена."
@@ -133,43 +141,25 @@ Translation.addTranslation("Modification is outgoing to produce? Let's compile a
 	ru: "Модификация подготовлена к производству? Давайте скомпилируем все, что мы разработали!"
 });
 
-LaunchSequence.__process = LaunchSequence.process;
-
 let debugIgnoreLockedBackground = false;
 
-LaunchSequence.process = function(index) {
-	if (index == 1) {
-		showHint.unstackLaunch();
-	}
-	let process = this.__process.apply(this, arguments);
-	if (index == 2) {
-		if (debugIgnoreLockedBackground) {
-			let popup = BitmapDrawableFactory.getMappedFileByKey("popup");
-			BitmapDrawableFactory.mapAs("popupSelectionLocked", popup);
-			BitmapDrawableFactory.mapAs("popupSelectionQueued", popup);
-		}
-		CHECKOUT("produce.js", null, function(who) {
-			AdditionalMessageFactory.registerClickable("menuProjectManage", translate("If you're wouldn't see development panel here, it may be removed."), 1, function(message) {
-				debugAttachControlTools = !debugAttachControlTools;
-				let control = message.getWindow();
-				control.removeElement(message);
-				control.removeElement(0);
-			}, function() {
-				return debugAttachControlTools;
-			});
-			AdditionalMessageFactory.registerClickable("explorerImport", translate("Modification is outgoing to produce? Let's compile anything that's we're developed!"), 0.5, function(message) {
-				REQUIRE("produce.js")(function() {
-					UniqueHelper.requireDestroy();
-					WindowProvider.destroy();
-					CHECKOUT("provider.js", null, function(who) {
-						attachEvalButton();
-					});
-				});
-			});
+AdditionalMessageFactory.registerClickable("menuProjectManage", translate("If you're wouldn't see development panel here, it may be removed."), 1, function(message) {
+	debugAttachControlTools = !debugAttachControlTools;
+	let control = message.getWindow();
+	control.removeElement(message);
+	control.removeElement(0);
+}, function() {
+	return debugAttachControlTools;
+});
+
+CHECKOUT("produce.js", function(who) {
+	AdditionalMessageFactory.registerClickable("explorerImport", translate("Modification is outgoing to produce? Let's compile anything that's we're developed!"), 0.5, function(message) {
+		REQUIRE("produce.js")(function() {
+			UniqueHelper.requireDestroy();
+			WindowProvider.destroy();
 		});
-	}
-	return process;
-};
+	});
+});
 
 SHARE({
 	debugAttachControlTools: {
@@ -178,7 +168,7 @@ SHARE({
 			return debugAttachControlTools;
 		},
 		set: function(v) {
-			debugAttachControlTools = Boolean(v);
+			debugAttachControlTools = !!v;
 		}
 	},
 	debugAttachBackground: {
@@ -187,7 +177,14 @@ SHARE({
 			return debugAttachBackground;
 		},
 		set: function(v) {
-			debugAttachBackground = Boolean(v);
+			debugAttachBackground = !!v;
+			CHECKOUT("interface/background.js", function(who) {
+				if (debugAttachBackground) {
+					attachBackground();
+				} else {
+					deattachBackground();
+				}
+			});
 		}
 	},
 	debugIgnoreLockedBackground: {
@@ -196,9 +193,24 @@ SHARE({
 			return debugIgnoreLockedBackground;
 		},
 		set: function(v) {
-			debugIgnoreLockedBackground = Boolean(v);
+			debugIgnoreLockedBackground = !!v;
+			if (debugIgnoreLockedBackground) {
+				let popup = BitmapDrawableFactory.getMappedFileByKey("popup");
+				BitmapDrawableFactory.mapAs("popupSelectionLocked", popup);
+				BitmapDrawableFactory.mapAs("popupSelectionQueued", popup);
+			}
 		}
 	}
 });
 
-CHECKOUT("development/explorer.js");
+function incontextualTest(scope) {
+	__debug_typecheck__(scope);
+	contextualTest(function(index) {
+		__debug_typecheck__(scope);
+		if (index == 2) {
+			print(scope.okayge);
+		}
+	});
+}
+
+SHARE("incontextualTest", incontextualTest);
