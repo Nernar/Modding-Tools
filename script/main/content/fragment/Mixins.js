54b37544f4dfe5cc7d27531ddecba4374d1a5c64
MaXFeeD <maksfeed.mf@gmail.com>
2024-10-11 13:30:03 +0300

    Added SelectableFragment mixin with explicit or layout selection types
    - By default, selectable fragments receives selection on clicking, which should call `toggle` in fragment.
    - In layout selection type select will be prevented if parent not explicitly set or `canSelectItem` returns false.
    - In explicit selection type select will ignore parenting completely, parent will be notified only after selection.
    - In denied selection type select will prevented in any case, `unselect` will be called when changing type.

--- script/main/content/fragment/Mixins.js
+++ script/main/content/fragment/Mixins.js
@@ -70,3 +70,126 @@ const ImageFragmentMixin = {
 		return this;
 	}
 };
+
+const SelectableFragment = function() {
+	MCSystem.throwException("Modding Tools: SelectableFragment is mixin abstraction!");
+};
+
+SelectableFragment.SELECTION_LAYOUT = 0;
+SelectableFragment.SELECTION_EXPLICIT = 1;
+SelectableFragment.SELECTION_DENIED = 2;
+
+SelectableFragment.prototype = {
+	getSelectionType() {
+		return this.selectionType || SelectableFragment.SELECTION_LAYOUT;
+	},
+	setSelectionType(type) {
+		if (type == null || this.getSelectionType() == type) {
+			return;
+		}
+		if (this.selected && type == SelectableFragment.SELECTION_DENIED) {
+			this.unselect();
+		}
+		this.selectionType = type;
+	},
+	isSelected() {
+		return this.selected || false;
+	},
+	select() {
+		if (this.selected || this.getSelectionType() == SelectableFragment.SELECTION_DENIED) {
+			return false;
+		}
+		let parent = this.getParent();
+		if (this.getSelectionType() == SelectableFragment.SELECTION_LAYOUT && (
+			parent == null || (parent.canSelectItem && !parent.canSelectItem(this))
+		)) {
+			return false;
+		}
+		this.selected = true;
+		this.onSelect && this.onSelect();
+		parent && parent.selectItemLayout && parent.selectItemLayout(this);
+		this.updateSelection();
+		return true;
+	},
+	unselect() {
+		if (!this.selected) {
+			return false;
+		}
+		let parent = this.getParent();
+		if (this.getSelectionType() == SelectableFragment.SELECTION_LAYOUT && (
+			parent == null || (parent.canUnselectItem && !parent.canUnselectItem(this))
+		)) {
+			return false;
+		}
+		this.selected = false;
+		this.onUnselect && this.onUnselect();
+		parent && parent.unselectItemLayout && parent.unselectItemLayout(this);
+		this.updateSelection();
+		return true;
+	},
+	toggle() {
+		if (this.selected) {
+			return this.unselect();
+		}
+		return this.select();
+	},
+	getSelectedBackground() {
+		return this.selectedBackground || null;
+	},
+	setSelectedBackground(src) {
+		this.selectedBackground = src;
+		this.selected && this.updateSelection();
+		return this;
+	},
+	getUnselectedBackground() {
+		return this.unselectedBackground || null;
+	},
+	setUnselectedBackground(src) {
+		this.unselectedBackground = src;
+		this.selected || this.updateSelection();
+		return this;
+	},
+	updateSelection() {
+		if (this.selected) {
+			this.setBackground(this.getSelectedBackground());
+		} else {
+			this.setBackground(this.getUnselectedBackground());
+		}
+		return this;
+	},
+	setOnSelectListener(listener) {
+		if (listener != null) {
+			this.onSelect = listener.bind(this);
+		} else {
+			delete this.onSelect;
+		}
+		return this;
+	},
+	setOnUnselectListener(listener) {
+		if (listener != null) {
+			this.onUnselect = listener.bind(this);
+		} else {
+			delete this.onUnselect;
+		}
+		return this;
+	}
+};
+
+SelectableFragment.parseJson = function(instanceOrJson, json) {
+	if (json.hasOwnProperty("selectionType")) {
+		instanceOrJson.setSelectionType(calloutOrParse(json, json.selectionType, [this, instanceOrJson]));
+	}
+	if (json.hasOwnProperty("selectedBackground")) {
+		instanceOrJson.setSelectedBackground(calloutOrParse(json, json.selectedBackground, [this, instanceOrJson]));
+	}
+	if (json.hasOwnProperty("unselectedBackground")) {
+		instanceOrJson.setUnselectedBackground(calloutOrParse(json, json.unselectedBackground, [this, instanceOrJson]));
+	}
+	if (json.hasOwnProperty("select")) {
+		instanceOrJson.setOnSelectListener(parseCallback(json, json.select, this));
+	}
+	if (json.hasOwnProperty("unselect")) {
+		instanceOrJson.setOnUnselectListener(parseCallback(json, json.unselect, this));
+	}
+};
+
